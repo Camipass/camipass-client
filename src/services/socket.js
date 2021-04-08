f7ea8e2646b6db3baa230974557861a1e88e739c
Carmine Conversano <conversano.carmine@protonmail.com>
2021-04-08 14:24:30 +0200

    env production

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import Cookies from "js-cookie";
 
-export const socket = io('http://localhost:8000', {
+export const socket = io(process.env.REACT_APP_SERVER_ADDRESS_WEBSOCKET, {
     withCredentials: true,
     extraHeaders: {
         'x-auth-token': Cookies.get(process.env.REACT_APP_COOKIENAME)

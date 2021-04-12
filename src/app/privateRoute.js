89344ef4002c599b0e93f9a0cf8eb08aa9910b9f
Mirko de Cillis <decillis.web@gmail.com>
2021-04-12 09:36:49 +0200

    set sweetalert on private route

--- src/app/privateRoute.js
+++ src/app/privateRoute.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import { useAuth } from "./auth";
+import swal from 'sweetalert2';
 
 export default function PrivateRoute({ children, ...rest }) {
     let auth = useAuth();
+
+    if (!auth.user) {
+        swal.fire({
+            titleText: "Accesso negato!",
+            text: "Per accedere a questa sezione devi aver effettuato il login",
+            icon: "error",
+            background: "#393B41",
+            confirmButtonColor: '#F95F72'
+        });
+    }
+
     return (
         <Route
             {...rest}

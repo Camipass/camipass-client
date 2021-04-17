6218d14d937c07e2de68ebb7cc27f745d750c668
Carmine Conversano <conversano.carmine@protonmail.com>
2021-04-17 18:42:41 +0200

    import fort awesome

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/app';
 import './style/style.css';
+import '@fortawesome/fontawesome-free/js';
 
 ReactDOM.render(
   <App />,

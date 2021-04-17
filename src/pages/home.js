1cee68c447b4249c92ba299ec545e96ebd6f9f20
Mirko de Cillis <decillis.web@gmail.com>
2021-04-17 12:46:27 +0200

    set homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import '../style/style.css';
+import React, {useEffect} from 'react';
+import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
 
 export default function Home() {
+
+    useEffect(() => {
+        bulmaCarousel.attach('#homecarousel', {
+            slidesToScroll: 1,
+            slidesToShow: 1,
+            autoplay: true,
+            loop: true,
+            infinite: true,
+            duration: 1000,
+            navigation: true
+        });
+    });
+
     return (
-        <h2>HOME</h2>
+        <div>
+            <div className='carousel carousel-animated carousel-animate-slide'>
+                <div className='carousel-container carouselhome' id="homecarousel">
+                    <img className="item-1" src="img/slide1.png" alt=""/>
+                    <img className="item-2" src="img/slide2.png" alt=""/>
+                    <img className="item-3" src="img/slide3.png" alt=""/>
+                </div>
+            </div>
+        </div>
     );
 }
--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -43,81 +43,75 @@ export default class Signup extends React.Component {
                         password.length >= 8 &&
                         username.length > 0;
         return (
-            <div className="columns" style={{paddingTop: "2em"}}>
-                <div className="column is-3">
-                {/*TODO: aggiungere robe in fase di registrazione*/}
-                </div>
-                <div className="column columns is-8 has-text-centered is-centered">
-                    <div className="column is-7-desktop is-12-mobile">
-                        <form id="signupForm" name="signupForm">
-                            <div className="field">
-                                <label className="is-one-third labelform" htmlFor="username"> Username </label>
-                                <div className="control has-icons-left">
-                                    <input className="input is-7" name="username" id="username"
-                                           value={this.state.username} required={true}
-                                           onChange={this.onInputChange.bind(this)}/>
-                                    <span className="iconField is-left">
-                                    <FontAwesomeIcon icon={faUser} size="2x"/>
-                                </span>
-                                </div>
+            <div className="columns has-text-centered is-centered" style={{paddingTop: "2em"}}>
+                <div className="column is-5-desktop is-12-mobile">
+                    <form id="signupForm" name="signupForm">
+                        <div className="field">
+                            <label className="is-one-third labelform" htmlFor="username"> Username </label>
+                            <div className="control has-icons-left">
+                                <input className="input is-7" name="username" id="username"
+                                       value={this.state.username} required={true}
+                                       onChange={this.onInputChange.bind(this)}/>
+                                <span className="iconField is-left">
+                                <FontAwesomeIcon icon={faUser} size="2x"/>
+                            </span>
                             </div>
-                            <div className="field">
-                                <label className="is-one-third labelform" htmlFor="email"> Email </label>
-                                <div className="control has-icons-left">
-                                    <input className="input" name="email" id="email" type="email"
-                                           value={this.state.email} required={true}
-                                           onChange={this.onInputChange.bind(this)}/>
-                                    <span className="iconField is-left">
-                                    <FontAwesomeIcon icon={faEnvelope} size="2x"/>
-                                </span>
-                                </div>
+                        </div>
+                        <div className="field">
+                            <label className="is-one-third labelform" htmlFor="email"> Email </label>
+                            <div className="control has-icons-left">
+                                <input className="input" name="email" id="email" type="email"
+                                       value={this.state.email} required={true}
+                                       onChange={this.onInputChange.bind(this)}/>
+                                <span className="iconField is-left">
+                                <FontAwesomeIcon icon={faEnvelope} size="2x"/>
+                            </span>
                             </div>
-                            <div className="field">
-                                <label className="is-one-third labelform" htmlFor="password"> Password </label>
-                                <div className="control has-icons-left">
-                                    <input className="input" name="password" id="password" type="password"
-                                           value={this.state.password} required={true}
-                                           onChange={this.onInputChange.bind(this)}/>
-                                    <span className="iconField is-left">
-                                    <FontAwesomeIcon icon={faKey} size="2x"/>
-                                </span>
-                                </div>
+                        </div>
+                        <div className="field">
+                            <label className="is-one-third labelform" htmlFor="password"> Password </label>
+                            <div className="control has-icons-left">
+                                <input className="input" name="password" id="password" type="password"
+                                       value={this.state.password} required={true}
+                                       onChange={this.onInputChange.bind(this)}/>
+                                <span className="iconField is-left">
+                                <FontAwesomeIcon icon={faKey} size="2x"/>
+                            </span>
                             </div>
-                            <div className="columns field is-grouped is-grouped-centered">
-                                <label htmlFor="color" className="column labelform">
-                                    Pick your color!
-                                    <div style={{
-                                        marginLeft: "0",
-                                        width: "2em",
-                                        height: "2em",
-                                        borderRadius: "50%",
-                                        border: "1px lightgray solid",
-                                        backgroundColor: this.state.color,
-                                    }}/>
-                                </label>
-                                <Circle className="column" onChange={this.handleChangeColor.bind(this)}/>
-                            </div>
-                            <div className="field is-grouped is-grouped-centered">
-                                <p className="control">
-                                    <input className="button is-primary" style={{margin: "auto"}}
-                                           type="button" value="Registrati!" id="submitSignup"
-                                           disabled={!enabled}
-                                           onClick={this.submit.bind(this)} />
-                                </p>
-                                <p className="control">
-                                    <input type="button" value="Reset" className="button is-light" onClick={ () => this.setState(this.defaultForm).bind(this) }/>
-                                </p>
+                        </div>
+                        <div className="columns field is-grouped is-grouped-centered">
+                            <label htmlFor="color" className="column labelform">
+                                Pick your color!
+                                <div style={{
+                                    marginLeft: "0",
+                                    width: "2em",
+                                    height: "2em",
+                                    borderRadius: "50%",
+                                    border: "1px lightgray solid",
+                                    backgroundColor: this.state.color,
+                                }}/>
+                            </label>
+                            <Circle className="column" onChange={this.handleChangeColor.bind(this)}/>
+                        </div>
+                        <div className="field is-grouped is-grouped-centered">
+                            <p className="control">
+                                <input className="button is-primary" style={{margin: "auto"}}
+                                       type="button" value="Registrati!" id="submitSignup"
+                                       disabled={!enabled}
+                                       onClick={this.submit.bind(this)} />
+                            </p>
+                            <p className="control">
+                                <input type="button" value="Reset" className="button is-light" onClick={ () => this.setState(this.defaultForm).bind(this) }/>
+                            </p>
 
-                            </div>
-                            <div className="field">
-                                <p className="control" style={{textAlign: "right"}}>
-                                    Ti sei già registratə? <Link to={"/login"}>Effettua il login</Link>.
-                                </p>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                        <div className="field">
+                            <p className="control" style={{textAlign: "right"}}>
+                                Ti sei già registratə? <Link to={"/login"}>Non perdere altro tempo</Link>.
+                            </p>
+                        </div>
+                    </form>
                 </div>
-
             </div>
         );
     }

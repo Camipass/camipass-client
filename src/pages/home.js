f3757dd82401bbeba47561579118a73ce83902ba
Carmine Conversano <conversano.carmine@protonmail.com>
2021-04-18 02:16:32 +0200

    performance improvement

--- src/pages/home.js
+++ src/pages/home.js
@@ -20,12 +20,12 @@ export default function Home() {
         <div>
             <div className='carousel carousel-animated carousel-animate-slide'>
                 <div className='carousel-container carouselhome' id="homecarousel">
-                    <img className="item-1" src="img/slide1.png" alt="Benvenutə in camipass"/>
-                    <img className="item-2" src="img/slide2.png"
+                    <img className="item-1" src="img/slide1.webp" alt="Benvenutə in camipass"/>
+                    <img className="item-2" src="img/slide2.webp"
                          alt="Chatta con tutti nella room globale... ...oppure entra in una room privata con i tuoi amici"/>
-                    <img className="item-3" src="img/slide3.png"
+                    <img className="item-3" src="img/slide3.webp"
                          alt="La privacy è importante per noi. Oltre a una connessione sicura, i messaggi non saranno salvati da noi &quot;Quello che accade nella room rimane nella room&quot;"/>
-                    <img className="item-3" src="img/slide4.png"
+                    <img className="item-3" src="img/slide4.webp"
                          alt="CARMINE CONVERSANO (Capo Chef) MIRKO DE CILLIS (Inserviente)"/>
                 </div>
             </div>
